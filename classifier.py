2bc2181734c1a671aca67f45d68b5d5c0b6bd92d
diegopenuela <diego.penuela@gmail.com>
2017-07-21 22:04:48 -0500

    answer_one

--- classifier.py
+++ classifier.py
@@ -11,8 +11,10 @@ def answer_zero():
 
 #Convert the sklearn.dataset cancer to a DataFrame.
 def answer_one():
-	one = pd.DataFrame(cancer.data , index=range(1, 570), columns=cancer.feature_names)
+	one = pd.DataFrame(cancer.data , columns=cancer.feature_names)
 	print (type(one))
+	print (one.index)
+	one.index = pd.RangeIndex(0, 569, 1)
 	print (one.index)
 	return one
 
